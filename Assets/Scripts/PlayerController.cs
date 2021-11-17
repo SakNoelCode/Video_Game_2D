d9973c3fa08b6d97a5d399add788303dbbd96296
sakCode <angelitotupapitomana@gmail.com>
2021-11-16 19:03:29 -0500

    Video_5

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,103 +4,189 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //---------------------------------------Variables---------------------------------------------------
+     
+    //[SerializeField] ==> Nos permite modificar el valor en el editor de  Unity
+    [Header("Valores Configurables")]
+    [SerializeField] private float velocidad;
+    [SerializeField] private float fuerzaSalto;
+    [SerializeField] private bool isSaltoMejorado;
+    [SerializeField] private float saltoLargo = 1.3f;
+    [SerializeField] private float saltoCorto = 1f;
+    [SerializeField] private Transform checkGround;
+    [SerializeField] private float checkGroundRadio;
+    [SerializeField] private LayerMask capaSuelo;
 
-    //Variables
-    public float velocidad;
-    public float velocidadMax;
-    public float fuerzaSalto;
-    public float friccionSuelo;
+    
+    [Header("Valores de Referencia")]
 
-    //Variable del colisionador Pies
-    public bool colPies = false;
+
+    //Variable para verificar si el player toca el suelo
+    private bool tocaSuelo = false;
 
     //Variable para poder modificar las propiedades de Player
     private Rigidbody2D rPlayer;
 
-    //Variable para obtener el valor de las animaciones
+    //Variable para poder obtener el valor de las animaciones
     private Animator aPlayer;
 
-    //Variable que toma el valor horizontal
+    //Variable flotante que tomara el valor del eje horizontal
     private float horizontal;
+
+    //Variable que tendrá dos valores(x,y) y será encargada de la velocidad del Player
+    private Vector2 nuevaVelocidad;
 
     //Variable inicializada con valor de True, el personaje inicia mirando a la derecha
     private bool miraDerecha = true;
-    
+
+    //Variable que se usa para verificar si el personaje esta saltando
+    private bool estaSaltando = false;
+
+    //Variable que se usa para verificar si el personaje puede saltar
+    private bool puedoSaltar = false;
+
+
+
+    //---------------------------------------Método Start---------------------------------------------------
     void Start()
     {
-        //a rPlayer le asignamos el componente rigiBody
+        //a rPlayer le asignamos el componente rigiBody, podremos modificar las propiedades de Player
         rPlayer = GetComponent<Rigidbody2D>();
-         
-        //a Player se le asigna el componente Animator
+
+        //a Player se le asigna el componente Animator, podremos modificar las propiedades Animator Player
         aPlayer = GetComponent<Animator>();
     }
 
-    
+
+    //---------------------------------------Método Update---------------------------------------------------
     void Update()
-    {
-        //Enviamos al método girarPlayer el valor del eje horizontal
-        girarPlayer(horizontal);
-
-        //Pasar la velocidad del jugador a la variable Velocidad en el eje X
-        aPlayer.SetFloat("VelocidadX", Mathf.Abs(rPlayer.velocity.x));
-
-        //Pasar la velocidad del jugador a la variable Velocidad en el eje Y
-        aPlayer.SetFloat("VelocidadY",(rPlayer.velocity.y));
-
-        //Pasar el valor de colPies a la Variable TocaSuelo
-        aPlayer.SetBool("TocaSuelo", colPies);
-
-        //Código para el salto
-        //Comprobar si estamos tocando Terreno
-        colPies = Pies.colPies;
-
-        if (Input.GetButton("Jump") && colPies)//Si seleccionamos la opcion de saltar
-        {
-            //Cambiamos la velocidad del Player en Y  en 0, de manera que si tuviera algun valor este se anule
-            rPlayer.velocity = new Vector2(rPlayer.velocity.x,0f);
-
-            //Añadimos la fuerza del salto al Player, el tipo de fuerza establecido como impulso
-            rPlayer.AddForce(new Vector2(0, fuerzaSalto),ForceMode2D.Impulse); 
-        }
-
-         
-
+    {       
+        recibePulsaciones();
+        variablesAnimador();
     }
 
+
+    //---------------------------------------Método Fixed Update---------------------------------------------------
     //Método encargado del sistema de fisicas de Unity
     void FixedUpdate()
+    {
+        checkTocaSuelo();
+        movimientoPlayer();
+    }
+
+     
+    private void movimientoPlayer()
+    {
+        //Player en el suelo
+        if(tocaSuelo && !estaSaltando)
+        {
+            nuevaVelocidad.Set(velocidad * horizontal, 0.0f);
+            rPlayer.velocity = nuevaVelocidad;
+        }
+
+        //Player Saltando
+        else
+        {
+            if (!tocaSuelo)
+            {
+                nuevaVelocidad.Set(velocidad * horizontal, rPlayer.velocity.y);
+                rPlayer.velocity = nuevaVelocidad;
+            }     
+        }
+        
+    }
+
+
+    //Método para controlar las teclas que pulsa el jugador
+    private void recibePulsaciones()
     {
         //La variable horizontal toma el valor del eje horizontal
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        //a rPlayer se le añade una fuerza hacia la derecha 
-        rPlayer.AddForce(Vector2.right * velocidad * horizontal);
+        //Se comprueba una condición para girar a rPlayer
+        if ((horizontal > 0 && !miraDerecha) || (horizontal < 0 && miraDerecha)) girarPlayer();
 
-        //Creamos un variable para que la velocidad tenga un valor mínimo y un valor máximo
-        float limiteVelocidad = Mathf.Clamp(rPlayer.velocity.x, -velocidadMax,velocidadMax);
+        //Si se oprime el boton se Salto se ejecuta la función Salto
+        if (Input.GetButton("Jump") && puedoSaltar) Salto();
 
-        //Asignamos esa velocidad al Player, la velocidad en el eje Y, lo dejamos tál cuál esta
-        rPlayer.velocity = new Vector2(limiteVelocidad,rPlayer.velocity.y);
+        //Si la variable isSaltoMejorado es verdadera, se ejecuta el salto Mejorado
+        if (isSaltoMejorado) SaltoMejorado();
+    }
 
-        //Codigo para arreglar la velocidad por friccion
-        if (horizontal == 0 && colPies)//Si no existe movimiento y si esta tocando el suelo
+
+    //Funcion que permite a Player ejecutar el salto
+    private void Salto()
+    {
+        estaSaltando = true;
+        puedoSaltar = false;
+
+        //Cambiamos la velocidad del Player en Y  en 0, de manera que si tuviera algun valor este se anule
+        rPlayer.velocity = new Vector2(rPlayer.velocity.x, 0f);
+
+        //Añadimos la fuerza del salto al Player, el tipo de fuerza establecido como impulso
+        rPlayer.AddForce(new Vector2(0, fuerzaSalto), ForceMode2D.Impulse);
+    }
+
+
+    //Función que permite hacer el salto mejorado
+    private void SaltoMejorado()
+    {
+        if (rPlayer.velocity.y < 0)
         {
-            //Aplicamos una friccion solo a la velocidad del eje x
-            Vector3 velocidadArreglada = rPlayer.velocity;
-            velocidadArreglada.x *= friccionSuelo;
-            rPlayer.velocity = velocidadArreglada;
+            rPlayer.velocity += Vector2.up * Physics2D.gravity.y * saltoLargo * Time.deltaTime;
+        }
+        else if (rPlayer.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rPlayer.velocity += Vector2.up * Physics2D.gravity.y * saltoCorto * Time.deltaTime;
         }
     }
+
+
+    //Función que permite verificar si el Player esta saltando o si puede Saltar
+    private void checkTocaSuelo()
+    {
+        tocaSuelo = Physics2D.OverlapCircle(checkGround.position, checkGroundRadio, capaSuelo);
+        if(rPlayer.velocity.y <= 0f)
+        {
+            estaSaltando = false;
+        }
+        if(tocaSuelo && !estaSaltando)
+        {
+            puedoSaltar = true;
+        }
+    }
+
+   
+    //Recoger los valores de las animaciones
+    private void variablesAnimador()
+    {
+        //Pasar la velocidad del jugador a la variable Velocidad en el eje X
+        aPlayer.SetFloat("VelocidadX", Mathf.Abs(rPlayer.velocity.x));
+
+        //Pasar la velocidad del jugador a la variable Velocidad en el eje Y
+        aPlayer.SetFloat("VelocidadY", (rPlayer.velocity.y));
+
+        //Asignar el valor de tocaSuelo a la variable TocaSuelo
+        aPlayer.SetBool("TocaSuelo", tocaSuelo);
+
+        //Asignar el valor de estaSaltando a la variable EstaSaltando
+        aPlayer.SetBool("EstaSaltando", estaSaltando);
+    }
+
+
 
     //Método para girar al Player de Derecha a Izquierda o Viceversa
-    void girarPlayer(float h)
-    {
-        if( (h>0 && !miraDerecha) || (h<0 && miraDerecha) )
-        {
-            miraDerecha = !miraDerecha;
-            Vector3 escalaGiro = transform.localScale;
-            escalaGiro.x = escalaGiro.x * -1;
-            transform.localScale = escalaGiro;
-        }
+    void girarPlayer()
+    {        
+        miraDerecha = !miraDerecha;
+        Vector3 escalaGiro = transform.localScale;
+        escalaGiro.x = escalaGiro.x * -1;
+        transform.localScale = escalaGiro;
     }
+
+
+   /* private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(checkGround.position, checkGroundRadio);
+    }*/
 } 
